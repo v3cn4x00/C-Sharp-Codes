ac91d11d3044a5a83c6d55dfb7ca886a4a5431c7
Muhammad Arafat <97059953+v3cn4x00@users.noreply.github.com>
2026-05-05 20:09:16 -0400

    Update vehicle.cs

--- vehicle.cs
+++ vehicle.cs
@@ -3,16 +3,13 @@ namespace Program
 {
     class Vehicle
     {
-        private string make;
+        private string? make;
         private int year;
         private int speed;
 
-        public string GetMake() { return make; }
-        public int GetYear() { return year; }
-        public int GetSpeed() { return speed; }
-        public void SetMake(string m) { make = m; }
-        public void SetYear(int y) { year = y; }
-        public void SetSpeed(int s) { speed = s; }
+        public string Make { get { return make; } set { make = value; } }
+        public int Year { get { return year; } set { year = value; } }
+        public int Speed { get { return speed; } set { speed = value; } }
     }
 
     internal class Program
@@ -20,17 +17,17 @@ namespace Program
         static void Main(string[] args)
         {
             Vehicle v1 = new Vehicle();
-            v1.SetMake("Honda"); v1.SetYear(1900); v1.SetSpeed(150);
+            v1.Make = "Honda"; v1.Year = 1900; v1.Speed = 150;
 
             Vehicle v2 = new Vehicle();
-            v2.SetMake("BMW"); v2.SetYear(2014); v2.SetSpeed(249);
+            v2.Make = "BMW"; v2.Year = 2014; v2.Speed = 249;
 
             Vehicle v3 = new Vehicle();
-            v3.SetMake("Lamborghini"); v3.SetYear(2025); v3.SetSpeed(350);
+            v3.Make = "Lamborghini"; v3.Year = 2025; v3.Speed = 350;
 
-            Console.WriteLine(v1.GetMake() + " " + v1.GetYear() + " " + v1.GetSpeed());
-            Console.WriteLine(v2.GetMake() + " " + v2.GetYear() + " " + v2.GetSpeed());
-            Console.WriteLine(v3.GetMake() + " " + v3.GetYear() + " " + v3.GetSpeed());
+            Console.WriteLine(v1.Make + " " + v1.Year + " " + v1.Speed);
+            Console.WriteLine(v2.Make + " " + v2.Year + " " + v2.Speed);
+            Console.WriteLine(v3.Make + " " + v3.Year + " " + v3.Speed);
         }
     }
 }
